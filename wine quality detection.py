7956c94b323233168c41a47561f9a76d13c010c0
riddhikahane <kahane.riddhi@gmail.com>
2026-06-22 22:37:05 +0530

    Create wine quality detection.py

---
+++ wine quality detection.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
